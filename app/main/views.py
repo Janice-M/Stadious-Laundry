977aa157f660ed916664a04805d809cb9394e891
Janice-M <janiceink001@gmail.com>
2019-08-15 12:31:24 +0300

    updated requirements.txt

--- app/main/views.py
+++ app/main/views.py
@@ -15,35 +15,35 @@ def index():
 
 def mapview():
 # creating a map in the view
-mymap = Map(
-identifier="view-side",
-lat=37.4419,
-lng=-122.1419,
-markers=[(37.4419, -122.1419)]
-)
-sndmap = Map(
-identifier="sndmap",
-lat=37.4419,
-lng=-122.1419,
-markers=[
-{
-'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-'lat': 37.4419,
-'lng': -122.1419,
-'infobox': "<b>Hello World</b>"
-},
-{
-'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-'lat': 37.4300,
-'lng': -122.1400,
-'infobox': "<b>Hello World from other place</b>"
-}
-]
-)
-return render_template('index.html', mymap=mymap, sndmap=sndmap)
+    mymap = Map(
+    identifier="view-side",
+    lat=37.4419,
+    lng=-122.1419,
+    markers=[(37.4419, -122.1419)]
+    )   
+    sndmap = Map(
+    identifier="sndmap",
+    lat=37.4419,
+    lng=-122.1419,
+    markers=[
+    {
+    'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+    'lat': 37.4419,
+    'lng': -122.1419,
+    'infobox': "<b>Hello World</b>"
+    },
+    {
+    'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+    'lat': 37.4300,
+    'lng': -122.1400,
+    'infobox': "<b>Hello World from other place</b>"
+    }
+    ]
+    )
+    return render_template('index.html', mymap=mymap, sndmap=sndmap)
 
-if __name__ == "__main__":
-app.run(debug=True)
+    if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
